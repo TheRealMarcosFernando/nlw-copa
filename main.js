ff7327bf17f6a4722d8fd6a1ae02e4f2eaf61314
Marcos Fernando <mft.jr@hotmail.com>
2022-11-06 14:47:54 -0300

    fix: data games

--- main.js
+++ main.js
@@ -22,8 +22,6 @@
  }
 
  document.querySelector("#cards").innerHTML =
-    createCard("21/11", "quinta", createGame("brazil", "16:00", "serbia")) +
-    createCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland") + createGame("portugal", "13:00", "uruguay")) +
+    createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+    createCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland")) +
     createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
- 
- 
